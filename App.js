bac8ecaf4e232098381837ffc608848d4151f239
Niraj Vishwakarma <neerajshree55@gmail.com>
2025-03-21 00:04:27 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const header = React.createElement(
     "h1",
     { id: "heading" },
@@ -26,8 +29,7 @@ const section1 = React.createElement(
                 "div",
                 { id: "child1" },
                 [
-                    React.createElement('h1', { id: 'heading1', abc: 'xyz' }, 'Child 1, Heading 1'),
-                    React.createElement('h2', { id: 'heading2' }, 'Child 1, Heading 2'),
+                    React.createElement('h2', { id: 'heading2' }, 'Lorem ipsum dolor'),
                     React.createElement('p', { id: 'paragraph' }, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
                     React.createElement('hr', {}),
                 ]
@@ -36,8 +38,7 @@ const section1 = React.createElement(
                 "div",
                 { id: 'child2' },
                 [
-                    React.createElement('h1', { id: 'heading1' }, "Child 2, Heading 1"),
-                    React.createElement('h2', { id: 'heading2' }, "Child 2, Heading 2"),
+                    React.createElement('h2', { id: 'heading2' }, "Lorem ipsum dolor"),
                     React.createElement('p', { id: 'paragraph' }, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
                 ]
             )
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste-React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code spliting
+- Differencial Bundling
+- Diagnostic
+- Error Handling
+- HTTPS hosting in development
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
